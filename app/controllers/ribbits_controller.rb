21b7f639fe368efce7c3df807454d74c7e2a7252
princi <princi.scp8@gmail.com>
2015-05-05 14:10:13 +0530

    added public ribbits

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,19 +1,21 @@
 class RibbitsController < ApplicationController
 
+  def index
+    @ribbits = Ribbit.all
+    @ribbit = Ribbit.new
+  end
 
   def create
-  ribbit = Ribbit.new(ribbit_params)
-  ribbit.user_id = current_user.id
+    ribbit = Ribbit.new(ribbit_params)
+    ribbit.user_id = current_user.id
 
-  flash[:error] = "Your Ribbit was over 140 characters" unless ribbit.save
-  redirect_to root_url
-
+    flash[:error] = "Your Ribbit was over 140 characters" unless ribbit.save
+    redirect_to root_url
   end
 
 
   def ribbit_params
     params.require(:ribbit).permit(:content, :length)
-
   end
 
 end
